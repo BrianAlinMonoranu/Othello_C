1d3ed4fbfc807aed5c5c8378d87a959fef5fc3fe
BrianAlinMonoranu <71988258+BrianAlinMonoranu@users.noreply.github.com>
2021-05-15 11:13:29 +0100

    Add files via upload

---
+++ Playable.h
@@ -0,0 +1,8 @@
+//
+// Created by brian on 04/05/2021.
+//
+#ifndef SOFTWARE_PLAYABLE_H
+#define SOFTWARE_PLAYABLE_H
+#define Max 16
+void moves(int value[4],char board[Max][Max], int current_player, int * check_1, int * check_2, int playable, int random);
+#endif //SOFTWARE_PLAYABLE_H
